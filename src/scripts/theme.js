f936bf1581c49546e1c683c1cfff5d350fd26e1f
denffv <denffv@yandex.ru>
2021-12-17 12:42:33 +0500

    fix menu

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -4,8 +4,12 @@
 (function($) {
     $(document).ready(function(){
 
-
         $(".header__burger").click(function(){
+            $(".header__burger, .nav").toggleClass("active");
+            $("body").toggleClass("lock");
+        });
+
+        $(".nav__item").click(function(){
             $(".header__burger, .nav").toggleClass("active");
             $("body").toggleClass("lock");
         });
